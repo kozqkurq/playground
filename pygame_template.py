d21ec444047588bd25e0e5c0fb3cd86de105f060
kozqkurq <kozqkurq>
2023-03-02 13:29:27 +0900

    テンプレート更新

--- pygame_template.py
+++ pygame_template.py
@@ -33,6 +33,12 @@ def main():
         pygame.display.update()
 
         # イベント受け取り
+        for event in pygame.event.get():
+
+            # 終了処理
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
 
                 
 
